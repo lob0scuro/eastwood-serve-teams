6e180f8a0706cd2816d8c765a4cec21f65b0ea7a
Cameron Lopez <kamrin717@gmail.com>
2025-06-26 12:14:49 -0500

    more changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 const dev = false;
 
-const URL = dev ? "http://127.0.0.1:5000" : "https://epcteams.com:8000";
+const URL = dev ? "http://127.0.0.1:5000" : "https://api.epcteams.com:8000";
 
 // https://vite.dev/config/
 export default defineConfig({
