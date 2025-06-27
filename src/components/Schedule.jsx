9dc4e70f3516ec6138ee921bccd3f27bf5984924
Cameron Lopez <kamrin717@gmail.com>
2025-06-27 10:12:06 -0500

    added logic for deleting volunteers

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,12 +1,25 @@
 import styles from "./Schedule.module.css";
 import React from "react";
 import { getSundaysInMonth, formatDate } from "../utils/tools";
+import { deleteVolunteer } from "../utils/API";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/UserContext";
 
 const Schedule = ({ volunteers }) => {
+  const { authenticated } = useAuth();
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth();
   const sundays = getSundaysInMonth(year, month);
+  const navigate = useNavigate();
+
+  const handleDelete = (id) => {
+    if (!confirm("Delete volunteer?")) {
+      return;
+    }
+    deleteVolunteer(id);
+    navigate("/");
+  };
 
   if (volunteers.length === 0) {
     return <p style={{ textAlign: "center" }}>No volunteers for this month</p>;
@@ -24,7 +37,13 @@ const Schedule = ({ volunteers }) => {
             <div className={styles.nameBlock}>
               {volunteersForThisWeek.length > 0 ? (
                 volunteersForThisWeek.map((person) => (
-                  <p key={person.id}>
+                  <p
+                    key={person.id}
+                    className={authenticated ? styles.volunteer : ""}
+                    onClick={
+                      authenticated ? () => handleDelete(person.id) : null
+                    }
+                  >
                     {person.position ? `${person.position}:` : null}
                     {person.name}
                   </p>
--- src/utils/API.jsx
+++ src/utils/API.jsx
@@ -2,3 +2,23 @@ const prod = true;
 export const server = prod
   ? "https://epcteams.com/api"
   : "http://localhost:5000";
+
+export const deleteVolunteer = async (id) => {
+  try {
+    const response = await fetch(`${server}/delete_volunteer/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error);
+    }
+    return { success: true, message: data.message };
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: error.message };
+  }
+};
