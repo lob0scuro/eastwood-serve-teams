abe86959c321e127b79dd752502331e388b19f75
Cameron Lopez <kamrin717@gmail.com>
2025-07-27 08:33:42 -0500

    hopefully fixed issue with volunteer form... lets see

--- src/components/Monther.jsx
+++ src/components/Monther.jsx
@@ -1,17 +1,25 @@
 import styles from "./Monther.module.css";
-import { useState } from "react";
 
 const Monther = ({ monthIndex, setMonthIndex }) => {
-  const monthName = new Date(0, monthIndex - 1).toLocaleString("defualt", {
+  const monthName = new Date(0, monthIndex - 1).toLocaleString("default", {
     month: "long",
   });
+
+  const handlePrev = () => {
+    setMonthIndex((prev) => (prev === 1 ? 12 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setMonthIndex((prev) => (prev === 12 ? 1 : prev + 1));
+  };
+
   return (
     <div className={styles.monther}>
-      <button type="button" onClick={() => setMonthIndex(monthIndex - 1)}>
+      <button type="button" onClick={handlePrev}>
         prev
       </button>
       <span>{monthName}</span>
-      <button type="button" onClick={() => setMonthIndex(monthIndex + 1)}>
+      <button type="button" onClick={handleNext}>
         next
       </button>
     </div>
